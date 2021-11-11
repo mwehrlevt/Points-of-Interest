4d9b48ea0c3626efae85483e12312c775806e2aa
Michelle <mwehrlevt@gmail.com>
2021-11-11 16:40:56 -0500

    Finished Chapter 4

---
+++ PointsOfInterest/ConversionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PointsOfInterest
+//
+//  Created by Michelle on 11/11/21.
+//  Copyright © 2021 mwehrle. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ConversionViewController loaded its view")
+    }
+}
+
---
+++ PointsOfInterest/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  PointsOfInterest
+//
+//  Created by Michelle on 11/11/21.
+//  Copyright © 2021 mwehrle. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view")
+    }
+}
